0ca8e513f926cab9b7b47eb4db36a429ce0c185d
anwar <anwarnsyd@gmail.com>
2025-06-10 13:39:39 +0530

    Bug Fixed

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import SelectTopic from './_components/SelectTopic'
 
-function page() {
+function CreateNew() {
+
+
+  const [formData,setFormData] = useState([])
+  const onHandleInputChange=(fieldName,fieldValue)=>{
+    console.log(fieldName,fieldValue)
+  }
+
   return (
-    <div>
-      Create New video
+    <div className='md:px-20'>
+      <h2 className='font-bold text-4xl text-primary text-center'>Create New</h2>
+
+      <div className='mt-10 shadow-md p-10'>
+        {/* Select Topic */}
+        <SelectTopic onUserSelect={onHandleInputChange}/>
+        {/* Select Style */}
+
+        {/* Duration */}
+
+        {/* Create Button */}
+
+      </div>
+
     </div>
   )
 }
 
-export default page
+export default CreateNew
