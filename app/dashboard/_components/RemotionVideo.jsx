96c3f5781a8919310a6d1b85bef8c7e234b1d13d
anwar <anwarnsyd@gmail.com>
2025-06-21 23:11:31 +0530

    animation effect

--- app/dashboard/_components/RemotionVideo.jsx
+++ app/dashboard/_components/RemotionVideo.jsx
@@ -1,5 +1,12 @@
+import { transform } from "next/dist/build/swc/generated-native";
 import React from "react";
-import { AbsoluteFill, Sequence, useCurrentFrame, useVideoConfig } from "remotion";
+import {
+  AbsoluteFill,
+  interpolate,
+  Sequence,
+  useCurrentFrame,
+  useVideoConfig,
+} from "remotion";
 
 function RemotionVideo({
   script,
@@ -9,54 +16,71 @@ function RemotionVideo({
   setDurationInFrame,
 }) {
   const { fps } = useVideoConfig();
-  const frame=useCurrentFrame()
+  const frame = useCurrentFrame();
   const getDureationFrame = () => {
     setDurationInFrame[(captions[captions?.length - 1]?.end / 1000) * fps];
     return (captions[captions?.length - 1]?.end / 100) * fps;
   };
 
-  const getCurrentCaptions=()=>{
-    const currentTime=frame/30*1000 // convert frame number into milliseconds {30fps}
+  const getCurrentCaptions = () => {
+    const currentTime = (frame / 30) * 1000; // convert frame number into milliseconds {30fps}
 
-    const currentCaption=captions.find((word)=>currentTime>=word.start && currentTime<=word.end)
+    const currentCaption = captions.find(
+      (word) => currentTime >= word.start && currentTime <= word.end
+    );
 
-    return currentCaption?currentCaption?.text:'';
-  }
+    return currentCaption ? currentCaption?.text : "";
+  };
 
   return (
     <AbsoluteFill className="bg-black">
       {imageList?.map((item, index) => {
-        <>
-          <Sequence
-            key={index}
-            from={(index * getDureationFrame()) / imageList?.length}
-            durationInFrames={getDureationFrame}
-          >
-            <AbsoluteFill
-              style={{ justifyContent: "center", alignItems: "center" }}
+
+        const startTime=(index * getDureationFrame()) / imageList?.length
+        const duration = getDureationFrame();
+
+
+        const scale =(index)=> interpolate(
+          frame,[startTime,startTime+duration/2,startTime+duration] , // zoom in & zoom out
+          index%2==0 ?[1,1.8,1]:[1.8,1,1.8],
+          {extrapolateLeft:'clamp',extrapolateRight:'clamp'}
+        )
+        return (
+          <>
+            <Sequence
+              key={index}
+              from={startTime}
+              durationInFrames={getDureationFrame}
             >
-              <Img
-                src={item}
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-              <AbsoluteFill style={{
-                color:'white',
-                justifyContent:'center',
-                top:undefined,
-                bottom:50,
-                height:150,
-                textAlign:'center',
-                width:'100%'
-              }}>
-                <h2 className="text-2xl">{getCurrentCaptions()}</h2>
+              <AbsoluteFill
+                style={{ justifyContent: "center", alignItems: "center" }}
+              >
+                <Img
+                  src={item}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    transform:`scale(${scale(index)})`
+                  }}
+                />
+                <AbsoluteFill
+                  style={{
+                    color: "white",
+                    justifyContent: "center",
+                    top: undefined,
+                    bottom: 50,
+                    height: 150,
+                    textAlign: "center",
+                    width: "100%",
+                  }}
+                >
+                  <h2 className="text-2xl">{getCurrentCaptions()}</h2>
+                </AbsoluteFill>
               </AbsoluteFill>
-            </AbsoluteFill>
-          </Sequence>
-        </>;
+            </Sequence>
+          </>
+        );
       })}
       <Audio src={audioFileUrl} />
     </AbsoluteFill>
